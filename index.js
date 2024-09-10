be682129c621b8dd37b88043dd80d6b2b6e54815
ikayisking <ikennaonyeukwu4@gmail.com>
2024-09-10 16:44:02 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const toTop = document.querySelector('.to-top');
+window.addEventListener ('scroll', () => {
+    if (window.pageYOffset > 100) {
+        toTop.classList.add('active');
+    } else {
+        toTop.classList.remove('active');
+    }
+})
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+function hideSidebar(){
+        const sidebar = document.querySelector('.sidebar')
+        sidebar.style.display = 'none'
+}
